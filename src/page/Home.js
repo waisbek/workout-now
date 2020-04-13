314f308ff89867fd8cf7d098fa929c6c1f76c3e4
Victor Vieira <victorvieira@Victors-MBP.Home>
2020-04-13 20:41:30 +0100

    Updated home page layout

--- src/page/Home.js
+++ src/page/Home.js
@@ -16,18 +16,37 @@ const Home = () => {
     const [goPreparation, setGoPreparation] = useState(false)
 
     return (
-        <Container className="d-flex home-page justify-content-center align-items-center" fluid>
-            <Row>
-                <Col xs="12">
-                    <div>
-                        <Label tag='h1'>WORKOUT NOW</Label>
-                    </div>
-                    <Jumbotron>
-                        {goPreparation && <Redirect to='/new-training' />}
-                        <Button block onClick={() => { setGoPreparation(true) }}>NOVO TREINAMENTO</Button>
-                    </Jumbotron>
-                </Col>
-            </Row>
+        <Container className="d-flex home-page justify-content-center align-items-center">
+            {goPreparation && <Redirect to='/new-training' />}
+            <Jumbotron className='new-training-page'>
+                <Row className='d-flex justify-content-center align-items-center'>
+                    <Label tag='h1'>WORKOUT NOW</Label></Row>
+                <Row className='d-flex justify-content-center align-items-center'>
+                    <img src="/assets/img/workout.svg" width="160" height="160" title="Workout" className='mr-2 mt-1' alt='Workout' />
+                </Row>
+                <Row className=' mt-3 d-flex justify-content-center align-items-center'>
+                    <ul>
+                        <li>
+                            <span className='empty-list-message'>Prepare o treino.</span>
+                        </li>
+                        <li>
+                            <span className='empty-list-message'>Crie rounds personalizados.</span>
+                        </li>
+                        <li>
+                            <span className='empty-list-message'>Defina os tempos.</span>
+                        </li>
+                        <li>
+                            <span className='empty-list-message'>Selecione os exercícios.</span>
+                        </li>
+                    </ul>
+                </Row>
+                <Row className='d-flex justify-content-center align-items-center'>
+                    <small>Clique no botão para começar.</small>
+                </Row>
+                <Row className='d-flex justify-content-center align-items-center'>
+                    <Button color="secondary" block onClick={() => { setGoPreparation(true) }}>NOVO TREINAMENTO</Button>
+                </Row>
+            </Jumbotron>
         </Container>
     )
 }
